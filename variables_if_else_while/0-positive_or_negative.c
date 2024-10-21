c877a60cad12cd0c3287cc21dc085fde0754453b
JaysonPasquier <jaysonpasquier.contact@gmail.com>
2024-10-21 14:21:31 +0200

    0

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,8 @@
 /**
  * main - Entry point of the program
  *
- * Description: Prints a quote using the puts function
+ * Description: Generates a random number and prints whether it's
+ * positive, negative, or zero.
  * Return: Always 0 (Success)
  */
 
@@ -17,15 +18,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-	  printf("%d is positive\n", n);
+		printf("%d is positive\n", n);
 	}
 	else if (n < 0)
 	{
-	  printf("%d is negative\n", n);
+		printf("%d is negative\n", n);
 	}
 	else
 	{
-	  printf("%d is zero\n", n);
+		printf("%d is zero\n", n);
 	}
 
 	return (0);
