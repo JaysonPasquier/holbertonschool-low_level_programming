e3e417282a0af032149cc6462cc937ee33783128
JaysonPasquier <jaysonpasquier.contact@gmail.com>
2024-10-21 18:35:40 +0200

    2

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints a quote using the puts function
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-  char alphabet = 'a';
+char alphabet = 'a';
 
-  while (alphabet <= 'z')
-    {
-      putchar(alphabet);
+while (alphabet <= 'z')
+{
+putchar(alphabet);
 
-      alphabet++;
-    }
-  putchar('\n')
-  
-  return (0);
+alphabet++;
+}
+putchar('\n')
+
+return (0);
 }
