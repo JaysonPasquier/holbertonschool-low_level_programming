657464d0b6b094fec890eaad6d0def23b5f556a8
JaysonPasquier <jaysonpasquier.contact@gmail.com>
2024-10-22 09:05:52 +0200

    0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints lowercase and uppercase alphabet
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+  putchar("_putchar\n")
+  
+  return
+}
