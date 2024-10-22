34cc6189cdd4a7d3ccbdc7a81d6e39728ec374d4
JaysonPasquier <jaysonpasquier.contact@gmail.com>
2024-10-22 10:17:40 +0200

    1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include "main.h"
 
-
-
 /**
  * print_alphabet - Prints the lowercase alphabet
  *
@@ -17,6 +15,7 @@ while (alphabet <= 'z')
 _putchar(alphabet);
 alphabet++;
 }
+_putchar('\n');
 }
 
 
