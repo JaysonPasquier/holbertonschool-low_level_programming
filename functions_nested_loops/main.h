58f37708f693c8af3b35896b491385cbbff69f03
JaysonPasquier <jaysonpasquier.contact@gmail.com>
2024-10-22 10:03:40 +0200

    1

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,6 +1,26 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+ * _putchar - Entry point of the program
+ *
+ * Description: Prints a quote using the puts function
+ * Return: Always 0 (Success)
+ */
+
 int _putchar(char c);
+
+void print_alphabet_x10(void)
+{
+char alphabet = 'a';
+
+while (alphabet <= 'z')
+{
+putchar(alphabet);
+
+alphabet++;
+}
+putchar('\n');
+}
 
 #endif
