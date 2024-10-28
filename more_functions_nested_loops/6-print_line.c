8f1ad05db71423efdd6e4e0a739f90e42d4bca8c
JaysonPasquier <jaysonpasquier.contact@gmail.com>
2024-10-28 12:04:14 +0100

    6

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,13 +1,24 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * print_line - print _ numbre time for n
+ * @n: the number for n
+ *
+ * Return: void
+ */
+
 void print_line(int n)
 {
 int i;
+
+if (n <= 0)
+_putchar('\n');
 
 for (i = 0; i < n; i++)
 {
 _putchar('_');
 }
+if (n > 0)
 _putchar('\n');
 }
