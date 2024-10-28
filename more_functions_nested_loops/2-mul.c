9a1002d2805f23aa7349a393ff019285eb4a01a9
JaysonPasquier <jaysonpasquier.contact@gmail.com>
2024-10-28 09:06:04 +0100

    2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul - multiply numbers
+ * @a : variable for the first number
+ * @b : variable for the seconde number
+ *
+ * Descritpion : multiply variable a and variable b
+ * Return: return 0
+ */
+
+int mul(int a, int b)
+{
+
+printf("%d\n", mul(a, b));
+
+return (0);
+}
