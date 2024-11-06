7a73b69961deeaee581efe218e156fb03205efac
JaysonPasquier <jaysonpasquier.contact@gmail.com>
2024-11-06 12:33:58 +0100

    6mod

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -8,13 +8,22 @@
  */
 int is_prime_number(int n)
 {
-int i;
+static int i = 2;
 if (n <= 1)
-return (0);
-for (i = 2; i * i <= n; i++)
 {
-if (n % i == 0)
+i = 2;
 return (0);
 }
+if (i * i > n)
+{
+i = 2;
 return (1);
+}
+if (n % i == 0)
+{
+i = 2;
+return (0);
+}
+i++;
+return (is_prime_number(n));
 }
