ecef725d3e76353b57b0657d372bf746cc97ddae
JaysonPasquier <jaysonpasquier.contact@gmail.com>
2024-11-06 12:28:53 +0100

    6 mod

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * is_prime_number - checks if a number is prime
  * @n: number to check
@@ -9,28 +8,26 @@
 int is_prime_number(int n)
 {
 static int divisor = 2;
-int is_prime;
-
 if (n <= 1)
 {
 divisor = 2;
 return (0);
 }
-
-if (divisor *divisor > n)
+if (n == 2)
 {
-is_prime = 1;
 divisor = 2;
-return (is_prime);
+return (1);
 }
-
+if (divisor * divisor > n)
+{
+divisor = 2;
+return (1);
+}
 if (n % divisor == 0)
 {
-is_prime = 0;
 divisor = 2;
-return (is_prime);
+return (0);
 }
-
 divisor++;
 return (is_prime_number(n));
 }
