5756df64b0f96de4ebb5333721d9aae6432f8ddb
JaysonPasquier <jaysonpasquier.contact@gmail.com>
2024-11-06 14:47:12 +0100

    0-whatsmyname

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/** 
+/**
+ * main - main function
+ * @argc: int argc
+ * @argv: char argv
  *
- *
- *
- *
- *
- *
+ * Description: print the name of the program
+ * Return: return 0
  */
 
 int main(int argc, char *argv[])
 {
-    (void)argc;
-    printf("%s\n", argv[0]);
-    return 0;
+(void)argc;
+printf("%s\n", argv[0]);
+return (0);
 }
