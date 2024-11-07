8a2290b31c2294db43e525f18eef8e37b56f2b39
JaysonPasquier <jaysonpasquier.contact@gmail.com>
2024-11-07 15:55:06 +0100

    "commit"

---
+++ argc_argv/teste.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * @argc: int argc
+ * @argv: char argv
+ *
+ * Description: print the number of argument given when compiling
+ * Return: return 0
+ */
+
+int main(int argc, char *argv[])
+{
+(void)argv;
+printf("%d\n", argc - 1);
+
+return (0);
+}
