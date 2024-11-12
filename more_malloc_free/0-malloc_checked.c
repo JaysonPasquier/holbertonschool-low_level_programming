b1b77484718cc95b9cd396196b0273913737aa44
JaysonPasquier <jaysonpasquier.contact@gmail.com>
2024-11-12 10:23:22 +0100

    0

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - main function
+ * @b: unsigned int b
+ *
+ * Description: allocates memory using malloc
+ * Return: return 98 or pointer t othe allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+
+void *ptr = malloc(b);
+
+if (ptr == NULL)
+exit (98);
+
+return (ptr);
+
+}
