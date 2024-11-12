89f57e8b5f227a2f6b55d08f9bab1dafb37062ac
JaysonPasquier <jaysonpasquier.contact@gmail.com>
2024-11-12 12:41:44 +0100

    2

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,5 +1,14 @@
 #include <stdlib.h>
 
+/**
+ * string_nconcat - main function
+ * @s1: char s1
+ * @s2: char s2
+ * @n: unsigned int n
+ *
+ * Description: concatenates two strings
+ * Return: result of NULL
+ */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 unsigned int len1 = 0;
@@ -8,30 +17,30 @@ unsigned int i;
 unsigned int j;
 char *result;
 
-if (s1 == NULL)
-s1 = "";
-if (s2 == NULL)
-s2 = "";
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-while (s1[len1] != '\0')
-len1++;
-while (s2[len2] != '\0')
-len2++;
+	while (s1[len1] != '\0')
+		len1++;
+	while (s2[len2] != '\0')
+		len2++;
 
-if (n >= len2)
-n = len2;
+	if (n >= len2)
+		n = len2;
 
-result = malloc((len1 + n + 1) * sizeof(char));
-if (result == NULL)
-return (NULL);
+	result = malloc((len1 + n + 1) * sizeof(char));
+	if (result == NULL)
+		return (NULL);
 
-for (i = 0; i < len1; i++)
-result[i] = s1[i];
+	for (i = 0; i < len1; i++)
+		result[i] = s1[i];
 
-for (j = 0; j < n; j++)
-result[i + j] = s2[j];
+	for (j = 0; j < n; j++)
+		result[i + j] = s2[j];
 
-result[i + j] = '\0';
+	result[i + j] = '\0';
 
-return (result);
+	return (result);
 }
