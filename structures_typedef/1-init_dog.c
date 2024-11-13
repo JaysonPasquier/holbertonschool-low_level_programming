9274c1e40fd69a8d8ab30070e125d0b096dae73a
JaysonPasquier <jaysonpasquier.contact@gmail.com>
2024-11-13 11:41:02 +0100

    1

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -8,11 +8,12 @@
  * @age: Âge du chien
  * @owner: Nom du propriétaire du chien
  */
-void init_dog(struct dog *d, char *name, float age, char *owner) {
-    if (d == NULL)
-        return;
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		return;
 
-    d->name = name;
-    d->age = age;
-    d->owner = owner;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
