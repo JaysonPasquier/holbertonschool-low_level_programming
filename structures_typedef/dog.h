5cc5beb7f38bf8d277f268bbf24ec8886667e144
JaysonPasquier <jaysonpasquier.contact@gmail.com>
2024-11-13 11:13:15 +0100

    dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,5 +1,17 @@
-typedef struct dog {
-   char *name;
-   float age;
-   char *owner;
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Structure définissant un chien avec des informations de base
+ * @name: Nom du chien
+ * @age: Âge du chien
+ * @owner: Propriétaire du chien
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
 } dog;
+
+#endif
