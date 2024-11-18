86124638a8ea0d8df71aba2e76f81ee3ee537e2c
JaysonPasquier <jaysonpasquier.contact@gmail.com>
2024-11-18 12:44:02 +0100

    1

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include <stddef.h>
+
+/**
+ * array_iterator - main function
+ * @array: int array
+ * @size: size_t size
+ * @action: void action
+ *
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	if (array != NULL && action != NULL)
+	{
+		size_t i;
+
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
+	}
+}
