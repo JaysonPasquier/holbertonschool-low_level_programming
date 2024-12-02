514d9b5a0ce16dda25aacc08eeb533474d74ecf5
JaysonPasquier <jaysonpasquier.contact@gmail.com>
2024-12-02 19:39:38 +0100

    maybe it will work

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -3,16 +3,17 @@
 #include "lists.h"
 
 /**
- * add_node - Adds a new node at the beginning of a list_t list
- * @head: Pointer to the pointer of the head of the list
- * @str: String to be added to the new node
+ * add_node - adds a new node at the beginning of a list_t list
+ * @head: pointer to the pointer of the head of the list
+ * @str: string to be added to the new node
  *
- * Return: Address of the new element, or NULL if it failed
+ * Return: the address of the new element, or NULL if it failed
  */
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
 	char *dup_str;
+	unsigned int len = 0;
 
 	if (head == NULL || str == NULL)
 		return (NULL);
@@ -28,27 +29,13 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	}
 
+	while (str[len])
+		len++;
+
 	new_node->str = dup_str;
-	new_node->len = strlen(str);
+	new_node->len = len;
 	new_node->next = *head;
 	*head = new_node;
 
 	return (new_node);
-}
-
-/**
- * free_list - Frees a list_t list
- * @head: Pointer to the head of the list
- */
-void free_list(list_t *head)
-{
-	list_t *temp;
-
-	while (head)
-	{
-		temp = head->next;
-		free(head->str);
-		free(head);
-		head = temp;
-	}
 }
