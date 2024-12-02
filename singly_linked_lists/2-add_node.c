dc222ddaa230a545e36507cdb81cd32168c89043
JaysonPasquier <jaysonpasquier.contact@gmail.com>
2024-12-02 19:28:35 +0100

    fixed the memory leak in task number 2

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -4,32 +4,51 @@
 
 /**
  * add_node - Adds a new node at the beginning of a list_t list
- * @head: Double pointer to the head of the list_t list
- * @str: String to add as the new node's content (duplicated)
+ * @head: Pointer to the pointer of the head of the list
+ * @str: String to be added to the new node
  *
- * Return: Address of the new element, or NULL if it fails
+ * Return: Address of the new element, or NULL if it failed
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new_node;
+    list_t *new_node;
+    char *dup_str;
 
-	if (str == NULL)
-		return (NULL);
+    if (head == NULL || str == NULL)
+        return (NULL);
 
-	new_node = malloc(sizeof(list_t));
-	if (new_node == NULL)
-		return (NULL);
+    new_node = malloc(sizeof(list_t));
+    if (new_node == NULL)
+        return (NULL);
 
-	new_node->str = strdup(str);
-	if (new_node->str == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
+    dup_str = strdup(str);
+    if (dup_str == NULL)
+    {
+        free(new_node);
+        return (NULL);
+    }
 
-	new_node->len = strlen(str);
-	new_node->next = *head;
-	*head = new_node;
+    new_node->str = dup_str;
+    new_node->len = strlen(str);
+    new_node->next = *head;
+    *head = new_node;
 
-	return (new_node);
+    return (new_node);
+}
+
+/**
+ * free_list - Frees a list_t list
+ * @head: Pointer to the head of the list
+ */
+void free_list(list_t *head)
+{
+    list_t *temp;
+
+    while (head)
+    {
+        temp = head->next;
+        free(head->str);
+        free(head);
+        head = temp;
+    }
 }
